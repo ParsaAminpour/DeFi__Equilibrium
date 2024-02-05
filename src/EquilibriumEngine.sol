4d3445c354fc5c09fba1e0758aec5ee443151890
ParsaAminpour <parsa.aminpour@gmail.com>
2024-02-05 14:30:34 +0330

    resolve a git issue

--- src/EquilibriumEngine.sol
+++ src/EquilibriumEngine.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {IERC20} from "@openzeppelin/contracts/tokens/ERC20/IERC20.sol";
+
 contract EquilibriumEngine {
     function depositCollateralAndMintDSCE() external returns (bool) {}
 
